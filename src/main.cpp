27b9e71f6ca8375219a1ea0d4c21221acc4dd24a
StefanKrax <stefan.krax@betasystems.com>
2025-04-01 23:27:42 +0200

    läuft...

--- src/main.cpp
+++ src/main.cpp
@@ -23,17 +23,15 @@ Pwm pwm = Pwm(); // Create a Pwm object
 //////////////////////////////////////
 #define SEACOCK_OPEN_PIN D0
 #define SEACOCK_CLOSE_PIN D1
-
 #define WATERSENESOR_PIN D3
 
 #define LIGHTS_HOPPELANDEKALLEKOJE_PIN 0
-#define LIGHTS_SALON_PORT_BUG_PIN 1
-#define LIGHTS_SALON_PORT_AFTER_PIN 2
-#define LIGHTS_PANTRY_OVEN_PIN 3
-#define LIGHTS_PANTRY_SINK_PIN 4
-#define LIGHTS_SALON_FLOOR_PIN 5
-
-
+#define LIGHTS_HOPPELANDEKALLEKOJE_LEDSTRIP_PIN 1
+#define LIGHTS_SALON_PORT_BUG_PIN 2
+#define LIGHTS_SALON_PORT_AFTER_PIN 3
+#define LIGHTS_PANTRY_OVEN_PIN 4
+#define LIGHTS_PANTRY_SINK_PIN 5
+#define LIGHTS_SALON_FLOOR_PIN 6
 //////////////////////////////////////
 
 using namespace sensesp;
@@ -58,84 +56,87 @@ void setup() {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
    // Digital Input Seacock Valve Open
   const uint8_t kDigitalInputSCVOPin = SEACOCK_OPEN_PIN;
-  const unsigned int kInterval = 1000;
+  const unsigned int kInterval = 10000;
+  pinMode(kDigitalInputSCVOPin, INPUT_PULLUP);
+  
+  auto digital_input_SCVO = std::make_shared<RepeatSensor<bool>>(
+    kInterval, [kDigitalInputSCVOPin]() { return digitalRead(kDigitalInputSCVOPin); });
 
-    pinMode(kDigitalInputSCVOPin, INPUT_PULLUP);
-    auto* digital_input_SCVO = new RepeatSensor<bool>(
-        kInterval,
-      [kDigitalInputSCVOPin]() { return digitalRead(kDigitalInputSCVOPin); });
-
-    // Connect digital input 2 to Signal K output.
-    digital_input_SCVO->connect_to(new SKOutputBool(
+  auto di_SCVO_metadata = std::make_shared<SKMetadata>(
+      "",  // No units for boolean values
+      "Seacock Valve Open State"  // Value description
+  );
+  auto di_SCVO_output = std::make_shared<SKOutputBool>(
       "sensors.Seacockvalve.Pantry.Open.value",          // Signal K path
       "/Sensors/Seacockvalve/Pantry/Open/Value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Seacock Valve Open State")  // Value description
-    ));
-
-
+      di_SCVO_metadata);
+  digital_input_SCVO->connect_to(di_SCVO_output);
 
   // Digital Input Seacock Valve Close
   const uint8_t kDigitalInputSCVCPin = SEACOCK_CLOSE_PIN;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
   pinMode(kDigitalInputSCVCPin, INPUT_PULLUP);
 
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  auto* digital_input_SCVC = new RepeatSensor<bool>(
-    kInterval,
-      [kDigitalInputSCVCPin]() { return digitalRead(kDigitalInputSCVCPin); });
+  auto digital_input_SCVC = std::make_shared<RepeatSensor<bool>>(
+    kInterval, [kDigitalInputSCVCPin]() { return digitalRead(kDigitalInputSCVCPin); });
   
-  // Connect digital input 2 to Signal K output.
-  digital_input_SCVC->connect_to(new SKOutputBool(
+  auto di_SCVC_metadata = std::make_shared<SKMetadata>(
+      "",  // No units for boolean values
+      "Seacock Valve Close State"  // Value description
+  );
+  auto di_SCVC_output = std::make_shared<SKOutputBool>(
       "sensors.Seacockvalve.Pantry.Close.value",          // Signal K path
       "/Sensors/Seacockvalve/Pantry/Close/Value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Seacock Valve Close State")  // Value description
-      ));
-
+      di_SCVC_metadata);
+  digital_input_SCVC->connect_to(di_SCVC_output);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Digital Input Water Sensor
-    const uint8_t kWATERSENESOR_PIN = WATERSENESOR_PIN;
-
+  const uint8_t kWATERSENESOR_PIN = WATERSENESOR_PIN;
   pinMode(kWATERSENESOR_PIN, INPUT_PULLUP);
 
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
+  auto digital_input_WS = std::make_shared<RepeatSensor<bool>>(
+    kInterval, [kWATERSENESOR_PIN]() { return digitalRead(kWATERSENESOR_PIN); });
 
-  auto* digital_input_WS = new RepeatSensor<bool>(
-    kInterval, 
-    [kWATERSENESOR_PIN]() { return digitalRead(kWATERSENESOR_PIN); });
-  
-  // Connect digital input 2 to Signal K output.
-  digital_input_WS->connect_to(new SKOutputBool(
+  auto di_WS_metadata = std::make_shared<SKMetadata>(
+      "",  // No units for boolean values
+      "Watersensor Pantry State"  // Value description
+  );
+  auto di_WS_output = std::make_shared<SKOutputBool>(
       "sensors.Watersensor.Pantry.value",          // Signal K path
       "/Sensors/Watersensor/Pantry/Value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Watersensor Pantry State")  // Value description
-      ));
-    
+      di_WS_metadata);
+  digital_input_WS->connect_to(di_WS_output);
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Part Lights:
 //
-// Hoppelandkallekoje
-auto lhlkk = new FloatSKListener("electrical.inside.hoppelandkallekoje.mid.light.value", CHANGE);
-auto* lhlkk_consumer = new LambdaConsumer<float>([](float input) {
-  pwm.setFrequency(LIGHTS_HOPPELANDEKALLEKOJE_PIN, 1000); // Set frequency to 1kHz
-  pwm.write(LIGHTS_HOPPELANDEKALLEKOJE_PIN, input);
-    debugI("Hoppelandkallekoje: %f", input);
-});
+// Hoppelandkallekoje Spot
+auto lhlkk = std::make_shared<SKValueListener<float>>(
+    "electrical.inside.hoppelandkallekoje.mid.light.value", CHANGE);
+    auto lhlkk_consumer = std::make_shared<LambdaConsumer<float>>(
+    [](float input) {
+      pwm.setFrequency(LIGHTS_HOPPELANDEKALLEKOJE_PIN, 1000); // Set frequency to 1kHz
+      pwm.write(LIGHTS_HOPPELANDEKALLEKOJE_PIN, input);
+      debugI("Hoppelandkallekoje Spot: %f", input);
+    });
 lhlkk->connect_to(lhlkk_consumer);
 
+
+// Hoppelandkallekoje LED Strip
+auto lhlkksl = std::make_shared<SKValueListener<float>>(
+    "electrical.inside.hoppelandkallekoje.ledstrip.light.value", CHANGE);
+auto lhlkksl_consumer = std::make_shared<LambdaConsumer<float>>(
+    [](float input) {
+      pwm.setFrequency(LIGHTS_HOPPELANDEKALLEKOJE_LEDSTRIP_PIN, 1000); // Set frequency to 1kHz
+      pwm.write(LIGHTS_HOPPELANDEKALLEKOJE_LEDSTRIP_PIN, input);
+      debugI("Hoppelandkallekoje Streifen: %f", input);
+    });
+lhlkksl->connect_to(lhlkksl_consumer);
+
 //Salon 1 Bug
-auto lsb = new SKValueListener<float>("electrical.inside.salon.port.bug.light.value", CHANGE);
+auto lsb = std::make_shared<SKValueListener<float>>(
+    "electrical.inside.salon.port.bug.light.value", CHANGE);
 auto* lsb_consumer = new LambdaConsumer<float>([](float input) {
     pwm.setFrequency(LIGHTS_SALON_PORT_BUG_PIN, 1000); // Set frequency to 1kHz
     pwm.write(LIGHTS_SALON_PORT_BUG_PIN, input);
@@ -144,26 +145,29 @@ auto* lsb_consumer = new LambdaConsumer<float>([](float input) {
 lsb->connect_to(lsb_consumer);
 
 // Salon 2 After
-auto lsa = new SKValueListener<float>("electrical.inside.salon.port.mid.light.value", CHANGE);
+auto lsa = std::make_shared<SKValueListener<float>>(
+    "electrical.inside.salon.port.after.light.value", CHANGE);
 auto* lsa_consumer = new LambdaConsumer<float>([](float input) {
     pwm.setFrequency(LIGHTS_SALON_PORT_AFTER_PIN, 1000); // Set frequency to 1kHz
     pwm.write(LIGHTS_SALON_PORT_AFTER_PIN, input);
-  debugI("Salon Port After: %f", input);
+    debugI("Salon Port After: %f", input);
 });
 lsa->connect_to(lsa_consumer);
 
 //Pantry
 //Oven
-auto lpo = new SKValueListener<float>("electrical.inside.pantry.oven.light.value", CHANGE);
+auto lpo = std::make_shared<SKValueListener<float>>(
+    "electrical.inside.pantry.oven.light.value", CHANGE);
 auto* lpo_consumer = new LambdaConsumer<float>([](float input) {
   pwm.setFrequency(LIGHTS_PANTRY_OVEN_PIN, 1000); // Set frequency to 1kHz
   pwm.write(LIGHTS_PANTRY_OVEN_PIN, input);  
     debugI("Pantry Oven: %f", input);
 });
-lsa->connect_to(lpo_consumer);
+lpo->connect_to(lpo_consumer);
 
 //Pantry Sink
-auto lps = new SKValueListener<float>("electrical.inside.pantry.sink.light.value", CHANGE);
+auto lps = std::make_shared<SKValueListener<float>>(
+    "electrical.inside.pantry.sink.light.value", CHANGE);
 auto* lps_consumer = new LambdaConsumer<float>([](float input) {
     pwm.setFrequency(LIGHTS_PANTRY_SINK_PIN, 1000); // Set frequency to 1kHz
     pwm.write(LIGHTS_PANTRY_SINK_PIN, input);
@@ -172,7 +176,8 @@ auto* lps_consumer = new LambdaConsumer<float>([](float input) {
 lps->connect_to(lps_consumer);
 
 //Salon Floor
-auto lsf = new SKValueListener<float>("electrical.inside.salon.floor.light.value", CHANGE);
+auto lsf = std::make_shared<SKValueListener<float>>(
+    "electrical.inside.salon.floor.light.value", CHANGE);
 auto* lsf_consumer = new LambdaConsumer<float>([](float input) {
     pwm.setFrequency(LIGHTS_SALON_FLOOR_PIN, 1000); // Set frequency to 1kHz
     pwm.write(LIGHTS_SALON_FLOOR_PIN, input);
